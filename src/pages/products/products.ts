baa5fbad832a8794d957002ef0a96c96fa222eec
David Carvalho <david.snt.carvalho@gmail.com>
2017-09-18 11:31:32 -0300

    Correção Products Page

--- src/pages/products/products.ts
+++ src/pages/products/products.ts
@@ -8,10 +8,63 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ProductsPage {
 
+  public products = [
+    {
+      description: 'Galaxy J5 Prime SM-G570M',
+      manufacturer: 'Samsung',
+      code: 1001,
+      price: 764.58,
+      thumbnail: 'assets/images/products/img1.jpg'
+    },
+    {
+      description: 'Galaxy J5 Duos SM-J500M/DS',
+      manufacturer: 'Samsung',
+      code: 1002,
+      price: 741.06,
+      thumbnail: 'assets/images/products/img2.jpg'
+    },
+    {
+      description: 'Zenfone Go Live ZB551KL',
+      manufacturer: 'Asus',
+      code: 1003,
+      price: 699,
+      thumbnail: 'assets/images/products/img3.jpg'
+    },
+    {
+      description: 'Galaxy J5 Prime SM-G570M',
+      manufacturer: 'Samsung',
+      code: 1001,
+      price: 764.58,
+      thumbnail: 'assets/images/products/img1.jpg'
+    },
+    {
+      description: 'Galaxy J5 Duos SM-J500M/DS',
+      manufacturer: 'Samsung',
+      code: 1002,
+      price: 741.06,
+      thumbnail: 'assets/images/products/img2.jpg'
+    },
+    {
+      description: 'Zenfone Go Live ZB551KL',
+      manufacturer: 'Asus',
+      code: 1003,
+      price: 699,
+      thumbnail: 'assets/images/products/img3.jpg'
+    },
+  ];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
+  }
+
+  onClick(product) {
+    console.log(product);
+  }
+
+  onButtonClick() {
+    console.log('Button Click');
   }
 
 }
