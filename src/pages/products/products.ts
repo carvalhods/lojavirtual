658d796bcbbba85dabf77d8c6e8733663e675ed1
David Carvalho <david.snt.carvalho@gmail.com>
2017-09-17 21:12:43 -0300

    Products Page

--- src/pages/products/products.ts
+++ src/pages/products/products.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the ProductsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-products',
@@ -19,7 +12,6 @@ export class ProductsPage {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ProductsPage');
   }
 
 }
